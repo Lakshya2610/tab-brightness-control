b61fb94dd23a70ce58e35f4ca2ac25b4bf8482f1
Lakshya2610 <lakshyagupta26@live.com>
2020-02-07 23:32:35 -0500

    Extension is now working as intended

--- public/content.js
+++ public/content.js
@@ -5,7 +5,7 @@ tag.style.backgroundColor = "black";
 tag.style.position = "fixed";
 tag.style.width = "100%";
 tag.style.height = "100%";
-tag.style.opacity = "0.4";
+tag.style.opacity = "0.0";
 tag.style.top = "0";
 tag.style.left = "0";
 tag.style.pointerEvents = "none";
@@ -13,8 +13,12 @@ tag.style.zIndex = String(Number.MAX_SAFE_INTEGER) + "";
 
 chrome.runtime.onConnect.addListener(function (p) {
     p.onMessage.addListener(function(msg) {
-        if(msg.type === "test") {
-            console.log('content script: test message recieved');
+        if(msg.type === "requestState") {
+            // report state to extension popup
+            p.postMessage({ type: "state", brightness: (1 - tag.style.opacity) * 100 });
+        }
+        if(msg.type === "updateBrightness") {
+            tag.style.opacity = String(1 - (msg.newValue / 100.0));
         }
     });
 })
--- src/App.js
+++ src/App.js
@@ -1,39 +1,92 @@
 /*global chrome*/
 import React from 'react';
-import logo from './logo.svg';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
 import './App.css';
+
+const PrettoSlider = withStyles({
+  root: {
+    color: '#52af77',
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus,&:hover,&$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
 
 class App extends React.Component {
 
   constructor() {
     super();
+    this.state = { sliderVal: 100 };
     this.port = null;
   }
 
   componentDidMount() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       this.port = chrome.tabs.connect(tabs[0].id);
-      console.log('Connected!');
+      // ask content script for last state
+      this.port.postMessage({ type: "requestState" });
+
+      this.port.onMessage.addListener((response) => {
+        if(response.type === "state") {
+
+          if(response.brightness !== this.state.sliderVal) { 
+            // set slider value according to the tab brightness
+            this.setState({ sliderVal: Math.round(Number(response.brightness)) });
+          }
+
+        }
+      });
     });
+  }
+
+  handleChange(event, value) {
+    this.port.postMessage({ type: "updateBrightness", newValue: value });
+    this.setState({ sliderVal: value });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <a style={{ color: "white", fontSize: "200%", fontWeight: "lighter", marginBottom: "0%", marginLeft: "32%", marginTop: "5%" }}>
+          Tab Brightness
+        </a>
+
+        <br></br>
+
+        <Brightness7Icon style={{ marginLeft: "5%", color: "white" }} />
+
+        <PrettoSlider 
+        valueLabelDisplay="auto" 
+        style={{ marginTop: "4%", marginLeft: "5%", width: "70%", color: "#1976d2" }} 
+        defaultValue={100}
+        value={this.state.sliderVal}
+        onChange={this.handleChange.bind(this)}
+        />
+
       </div>
     );
   }
